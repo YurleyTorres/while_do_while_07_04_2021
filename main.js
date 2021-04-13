918a13d1c54151e29acd5122ca78a7e309b8baf8
YurleyTorres <marly.torres.silva@gmail.com>
2021-04-12 19:54:37 -0500

    07-04-2021 while

---
+++ main.js
@@ -0,0 +1,26 @@
+console.log("Ciclos");
+
+//Ciclo FOR
+// for (let i = 0; i < 3; i++) {
+//     console.log(i);
+// }
+
+// for (let i = ''; !(i == 'Marly Yurley Torres Silva');) {
+//     i = prompt("Ingrese su nombre");
+// }
+//ciclo while
+// let i = 0, x = 0;
+// while(i<3 && x<3){
+//     console.log(i);
+//     console.log(`Variable constante ${x}`);
+//     i++;
+// }
+// ciclo do while
+// let opc = '';
+// while(!(opc=="Marly Yurley Torres Silva")){
+//     opc = prompt("Ingrese su nombre");
+// }
+
+// do{
+//     var opc = prompt("Ingrese su nombre");
+// }while(!(opc=="Marly Yurley Torres Silva"));
